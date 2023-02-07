4924e6d24d03871fccfec79d0f0cd195150f1991
Thomas T <tranthom618@gmail.com>
2023-02-07 08:38:21 -0500

    API fetch function created and tested in console

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,16 @@
 // DOM Elements List
 var timeDisplayEl = $('#time-display');
+var weather = {
+   "APIweather": "042a86fbb574cb5568466fe7eb249647",
+   fetchWeather: function() {
+    fetch("http://api.openweathermap.org/data/2.5/forecast?q=toronto&units=metric&appid=042a86fbb574cb5568466fe7eb249647"
+    ).then((response) => response.json())
+    .then((data) => console.log(data))
+   }
+}
 
 
-var APIweather = "042a86fbb574cb5568466fe7eb249647";
+
 
 // Displays date and time
 function displayTime() {
