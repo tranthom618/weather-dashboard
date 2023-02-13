bde29f74332662b5e3558d1e354ad7b854794c2d
Thomas T <tranthom618@gmail.com>
2023-02-13 01:47:47 -0500

    Forecast Function Completed - For Loop to Cycle through API data + makeshift loop/if statement for children text fill

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,12 @@
 // DOM Elements List
-var timeDisplayEl = $('#time-display');
+let timeDisplayEl = $('#time-display');
 const APIweather = "042a86fbb574cb5568466fe7eb249647"
-var searchHistory = ["Toronto", "Vancouver", "Montreal", "New York", "Los Angeles"];
+let searchHistory = ["Toronto", "Vancouver", "Montreal", "New York", "Los Angeles"];
+let forecastEl = document.querySelector("#forecast-card");
 
 
 
-// 5 Day Forecast API Call
+//----- 5 Day Forecast API Call -----//
 var weatherForecast = {
 
     // Fetch Function
@@ -23,94 +24,38 @@ var weatherForecast = {
 },
 
     // Data endpoints for 1 Day Ahead. [6] is approx 24 hours worth of indexes ahead. [0] is 6 ahead, [1-6] is 3 each
-    display1DayWeather: function (data) {
-    const { name } = data.city;
-    const { icon, description } = data.list[6].weather[0];
-    const { temp, humidity } = data.list[6].main;
-    const { speed } = data.list[6].wind;
+    displayForecast: function (data) {
 
-    // Uses querySelector to select html .elements then adds the above variables within strings to display
-    document.querySelector(".city").innerText = name + " - Current Weather Conditions";
-    document.querySelector(".icon").src = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
-    document.querySelector(".temp").innerText = temp + "°C";
-    document.querySelector(".description").innerText = description.toUpperCase();
-    document.querySelector(".wind").innerText = "Wind Speed: " + speed + " km/hr";
-    document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
-   
-},
+        // Using j as a 'destructured' for loop to cycle through each children of each day of 'Forecast' card.
+        let j = 0;
 
-    // Data endpoints for 2 Days Ahead. [14] minus [6] is 8 total indexes over, with each being 3 hours. 8x3 = 24 hours
-    display2DayWeather: function (data) {
-        const { name } = data.city;
-        const { icon, description } = data.list[14].weather[0];
-        const { temp, humidity } = data.list[14].main;
-        const { speed } = data.list[14].wind;
-    
-        // Uses querySelector to select html .elements then adds the above variables within strings to display
-        document.querySelector(".city").innerText = name + " - Current Weather Conditions";
-        document.querySelector(".icon").src = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
-        document.querySelector(".temp").innerText = temp + "°C";
-        document.querySelector(".description").innerText = description.toUpperCase();
-        document.querySelector(".wind").innerText = "Wind Speed: " + speed + " km/hr";
-        document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
-       
-},
+        // For Loop to cycle through the data with i being used to reference the index for the API array reponse
+        // Starts at [7] because that is 6:00 am tomorrow, [39] is 6 am five days from now, so set to 40 to ensure all data is looped. 
+        // 8 is added to account for 24 hour periods (3hr per index)
+        for (let i = 7; i < 40; i += 8) {
+            const { dt_txt } = data.list[i];
+            const { icon, description } = data.list[i].weather[0];
+            const { temp, humidity } = data.list[i].main;
+            const { speed } = data.list[i].wind;
 
-    // Data endpoints for 3 Days Ahead. [22] is 8 total indexes after, with each being 3 hours. 8x3 = 24 hours
-    display3DayWeather: function (data) {
-        const { name } = data.city;
-        const { icon, description } = data.list[22].weather[0];
-        const { temp, humidity } = data.list[22].main;
-        const { speed } = data.list[14].wind;
-    
-        // Uses querySelector to select html .elements then adds the above variables within strings to display
-        document.querySelector(".city").innerText = name + " - Current Weather Conditions";
-        document.querySelector(".icon").src = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
-        document.querySelector(".temp").innerText = temp + "°C";
-        document.querySelector(".description").innerText = description.toUpperCase();
-        document.querySelector(".wind").innerText = "Wind Speed: " + speed + " km/hr";
-        document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
-       
-},
-
-    // Data endpoints for 4 Days Ahead. [30] is 8 total indexes after, with each being 3 hours. 8x3 = 24 hours
-    display4DayWeather: function (data) {
-        const { name } = data.city;
-        const { icon, description } = data.list[30].weather[0];
-        const { temp, humidity } = data.list[30].main;
-        const { speed } = data.list[30].wind;
-    
-        // Uses querySelector to select html .elements then adds the above variables within strings to display
-        document.querySelector(".city").innerText = name + " - Current Weather Conditions";
-        document.querySelector(".icon").src = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
-        document.querySelector(".temp").innerText = temp + "°C";
-        document.querySelector(".description").innerText = description.toUpperCase();
-        document.querySelector(".wind").innerText = "Wind Speed: " + speed + " km/hr";
-        document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
-       
-},
-
-    // Data endpoints for 5 Days Ahead. [38] is 8 total indexes after, with each being 3 hours. 8x3 = 24 hours
-    display4DayWeather: function (data) {
-        const { name } = data.city;
-        const { icon, description } = data.list[38].weather[0];
-        const { temp, humidity } = data.list[38].main;
-        const { speed } = data.list[38].wind;
-    
-        // Uses querySelector to select html .elements then adds the above variables within strings to display
-        document.querySelector(".city").innerText = name + " - Current Weather Conditions";
-        document.querySelector(".icon").src = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
-        document.querySelector(".temp").innerText = temp + "°C";
-        document.querySelector(".description").innerText = description.toUpperCase();
-        document.querySelector(".wind").innerText = "Wind Speed: " + speed + " km/hr";
-        document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
-       
+            if (j < 5) {
+                // Uses children to select html .elements then adds the above variables within strings to display
+                forecastEl.children[j].children[0].innerText = dt_txt;
+                forecastEl.children[j].children[1].innerText = temp + "°C";
+                forecastEl.children[j].children[2].src = "http://openweathermap.org/img/wn/" + icon + ".png";
+                forecastEl.children[j].children[3].innerText = description.toUpperCase();
+                forecastEl.children[j].children[4].innerText = "Wind Speed: \n" + speed + " km/hr";
+                forecastEl.children[j].children[5].innerText = "Humidity: " + humidity + "%";
+                j += 1;
+                console.log(dt_txt);
+            }
+    }
 },
 }
 
 
 
-// Current Weather API Call
+//----- Current Weather API Call -----//
 var weatherCurrent = {
 
     // Fetch Function
@@ -146,15 +91,14 @@ var weatherCurrent = {
 
 }
 
-
-function search() {
-    let searchCity = document.querySelector(".search-bar").value
-    searchHistory.unshift(searchCity); // Unshift to add to beginning of array.
+//----- Search Function -----//
+function search(city) {
+    searchHistory.unshift(city); // Unshift to add to beginning of array.
     searchHistory.pop(); // Pop will stop the array from infinitely building. Add (unshift) +1 at [0] then remove (pop) -1 the last in the array
 
     // Calls both the curret and future forecast functions using the city being searched. Connected to their respective fetch functions as well.
-    // weatherForecast.fetchForecast(searchCity);
-    weatherCurrent.fetchWeather(searchCity);
+    weatherForecast.fetchForecast(searchCity);
+    weatherCurrent.fetchWeather(city);
 
     //Save the search history array to local storage
     localStorage.setItem("localStorageSearch", JSON.stringify(searchHistory));    
@@ -167,6 +111,7 @@ function search() {
     document.querySelector('#prevCities4').textContent = searchHistory[4];
 }
 
+//----- Init Function to Load Defaults and Locally Saved Search History -----//
 function init () {
 
     // Retrieve the local storage data from the client-side storage
@@ -179,9 +124,9 @@ function init () {
     document.querySelector('#prevCities3').textContent = searchHistory[3];
     document.querySelector('#prevCities4').textContent = searchHistory[4];
 
-    // Calls both the curret and future forecast functions using the city being searched. From init(), it will default load the first saved city from search history.
-    // weatherForecast.fetchForecast(searchHistory[0]);
-    weatherCurrent.fetchWeather(searchHistory[0]);
+    // Calls both the curret and future forecast functions using the city being searched. From init(), it will default load Toronto.
+    weatherForecast.fetchForecast("Toronto");
+    weatherCurrent.fetchWeather("Toronto");
 }
 init();
 
@@ -189,7 +134,7 @@ init();
 
 
 
-// Displays date and time
+//----- Displays date and time -----//
 function displayTime() {
     var rightNow = dayjs().format('MMM DD, YYYY [at] hh:mm:ss a');
     timeDisplayEl.text(rightNow);
@@ -200,62 +145,55 @@ setInterval(displayTime, 1000);
 
 
 
-// Search Button Event Listener
+//----- Search Form/Button Event Listeners -----//
+// Search-Button Event Listener
 document.querySelector(".search button").addEventListener("click", function() {
-    search();
+    let searchCity = document.querySelector(".search-bar").value
+    search(searchCity);
 })
 
-// Search Bar "Enter" Key Event Listener
+// Search-Bar "Enter" Key Event Listener
 document.querySelector(".search-bar").addEventListener("keyup", function(event) {
     if (event.key == "Enter") {
-        search();
+        let searchCity = document.querySelector(".search-bar").value
+        search(searchCity);
     }
 })
 
 
 
-// Event listeners for Group of Buttons for Search History
+//----- Event listeners for Group of Buttons for Search History -----//
 // First Button of Search History
 document.querySelector("#prevCities0").addEventListener("click", function() {
-    let cityHist = searchHistory[0]; // Retrieves the first city saved in the array
-
     // Calls the fetch functions for current weather and forecast with the first search history button/array
-    // weatherForecast.fetchForecast(cityHist);
-    weatherCurrent.fetchWeather(cityHist);
+    weatherForecast.fetchForecast(searchHistory[0]);
+    weatherCurrent.fetchWeather(searchHistory[0]);
 })
 
 // Second Button of Search History
 document.querySelector("#prevCities1").addEventListener("click", function() {
-    let cityHist = searchHistory[1]; // Retrieves the first city saved in the array
-
-    // Calls the fetch functions for current weather and forecast with the first search history button/array
-    // weatherForecast.fetchForecast(cityHist);
-    weatherCurrent.fetchWeather(cityHist);
+    // Calls the fetch functions for current weather and forecast with the second search history button/array
+    weatherForecast.fetchForecast(searchHistory[1]);
+    weatherCurrent.fetchWeather(searchHistory[1]);
 })
 
 // Third Button of Search History
 document.querySelector("#prevCities2").addEventListener("click", function() {
-    let cityHist = searchHistory[2]; // Retrieves the first city saved in the array
-
-    // Calls the fetch functions for current weather and forecast with the first search history button/array
-    // weatherForecast.fetchForecast(cityHist);
-    weatherCurrent.fetchWeather(cityHist);
+    // Calls the fetch functions for current weather and forecast with the third search history button/array
+    weatherForecast.fetchForecast(searchHistory[2]);
+    weatherCurrent.fetchWeather(searchHistory[2]);
 })
 
 // Fourth Button of Search History
 document.querySelector("#prevCities3").addEventListener("click", function() {
-    let cityHist = searchHistory[3]; // Retrieves the first city saved in the array
-
-    // Calls the fetch functions for current weather and forecast with the first search history button/array
-    // weatherForecast.fetchForecast(cityHist);
-    weatherCurrent.fetchWeather(cityHist);
+    // Calls the fetch functions for current weather and forecast with the fourth search history button/array
+    weatherForecast.fetchForecast(searchHistory[3]);
+    weatherCurrent.fetchWeather(searchHistory[3]);
 })
 
 // Fifth Button of Search History
 document.querySelector("#prevCities4").addEventListener("click", function() {
-    let cityHist = searchHistory[4]; // Retrieves the first city saved in the array
-
-    // Calls the fetch functions for current weather and forecast with the first search history button/array
-    // weatherForecast.fetchForecast(cityHist);
-    weatherCurrent.fetchWeather(cityHist);
+    // Calls the fetch functions for current weather and forecast with the fifth search history button/array
+    weatherForecast.fetchForecast(searchHistory[4]);
+    weatherCurrent.fetchWeather(searchHistory[4]);
 })
